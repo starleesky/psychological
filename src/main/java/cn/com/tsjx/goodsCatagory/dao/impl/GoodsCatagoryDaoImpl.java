4ae7d19cd977d456b991b7859760900466bbde0c
liaoxianghua <821866756@qq.com>
2016-03-18 08:37:22 +0800

    src

--- src/main/java/cn/com/tsjx/goodsCatagory/dao/impl/GoodsCatagoryDaoImpl.java
+++ src/main/java/cn/com/tsjx/goodsCatagory/dao/impl/GoodsCatagoryDaoImpl.java
@@ -2,6 +2,8 @@ package cn.com.tsjx.goodsCatagory.dao.impl;
 
 import org.springframework.stereotype.Repository;
 
+import com.hp.hpl.sparta.xpath.ThisNodeTest;
+
 import cn.com.tsjx.common.dao.BaseDaoImpl;
 import cn.com.tsjx.goodsCatagory.dao.GoodsCatagoryDao;
 import cn.com.tsjx.goodsCatagory.entity.GoodsCatagory;

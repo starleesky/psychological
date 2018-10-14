6677b0b65e077b6a93ffaab4f49c4775b0d55765
liaoxianghua <821866756@qq.com>
2018-10-14 22:27:14 +0800

    修改错误方案 去掉定时任务

--- src/main/java/cn/com/tsjx/user/task/UserTask.java
+++ src/main/java/cn/com/tsjx/user/task/UserTask.java
@@ -1,22 +1,20 @@
 package cn.com.tsjx.user.task;
 
-import javax.annotation.Resource;
-
-import org.springframework.scheduling.annotation.Scheduled;
+import cn.com.tsjx.user.service.UserService;
 import org.springframework.stereotype.Component;
 
-import cn.com.tsjx.user.service.UserService;
+import javax.annotation.Resource;
 
 @Component
 public class UserTask {
 
 	@Resource
 	UserService userService;
-	
+
 	/**
 	 * 自动删除未及时激活的注册用户
 	 */
-	@Scheduled(cron="0 0/10 * * * ? ") //间隔10分钟执行
+//	@Scheduled(cron="0 0/10 * * * ? ") //间隔10分钟执行
 	public void autoDeteleUser() {
 		userService.autoDeteleUser();
 	}

7fa56c67ccd2a68a643f7008a82a09b3760acc25
liaoxianghua <821866756@qq.com>
2018-10-28 07:23:07 +0800

    添加微信用户实体

---
+++ src/main/java/cn/com/eap/service/EapWeixinUserService.java
@@ -0,0 +1,11 @@
+package cn.com.eap.service;
+
+import cn.com.tsjx.common.service.BaseService;
+import cn.com.eap.entity.EapWeixinUser;
+
+/**
+ * 服务接口。
+ */
+public interface EapWeixinUserService extends BaseService<EapWeixinUser, Long> {
+
+}
